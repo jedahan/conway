135e2a0eb5bf328d5e8c04098977fdd2072c8681
Jonathan Dahan <jonathan@jonathan.is>
2015-11-10 21:14:50 -0500

    implement first, super basic version

---
+++ readme.md
@@ -0,0 +1,3 @@
+# conway - a server that calculates the next step for a game of life
+
+Install with `npm install`, run with `npm start`, test with `npm test`.
---
+++ server.js
@@ -0,0 +1,60 @@
+'use strict'
+
+const koa = require('koa')
+const response_time = require('koa-response-time')
+const logger = require('koa-logger')
+const cors = require('koa-cors')
+const app = koa()
+require('koa-safe-jsonp')(app)
+require('koa-qs')(app)
+
+console.log('Environment:' + app.env)
+
+app.use(response_time())
+app.use(logger())
+app.use(cors())
+
+app.use(function* () {
+  const m = + this.query.M
+  const n = + this.query.N
+  const cells = new Array(m)
+  for(let i=0; i < m; i++){
+    cells[i] = new Array(n,0)
+  }
+
+  let liveCells = this.query.liveCells
+  for(let [x, y] of liveCells){
+    cells[+x][+y] = 1
+  }
+
+  const neighbors = function(x, y) {
+    let sum = 0
+    sum += (cells[x-1] && cells[x-1][y-1] ? 1 : 0)
+    sum += (cells[x-1] && cells[x-1][y] || 0)
+    sum += (cells[x-1] && cells[x-1][y+1] ? 1 : 0)
+    sum += (cells[x][y-1] || 0)
+    sum += (cells[x][y+1] || 0)
+    sum += (cells[x+1] && cells[x+1][y-1] ? 1 : 0)
+    sum += (cells[x+1] && cells[x+1][y] ? 1 : 0)
+    sum += (cells[x+1] && cells[x+1][y+1] ? 1 : 0)
+    return sum
+  }
+
+  liveCells = []
+  for(let x=0; x < m; x++){
+    for(let y=0; y < n; y++) {
+      const friends = neighbors(x,y)
+      if(friends === 3 || (friends === 2 && cells[x][y])) {
+        liveCells.push([x,y])
+      }
+    }
+  }
+
+  this.jsonp = liveCells
+})
+
+app.listen(process.env.PORT || 5000, function() {
+  const key = this._connectionKey.split(':')
+  const port = key[key.length - 1]
+  return console.log(`[${process.pid}] listening on :${port}`)
+})
