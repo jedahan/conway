86d1209727409f0b7bfe782245a11f478e28eace
Jonathan Dahan <jonathan@jonathan.is>
2015-11-13 15:29:55 -0500

    fix neighbor checking

--- readme.md
+++ readme.md
@@ -1,3 +1,5 @@
 # conway - a server that calculates the next step for a game of life
 
 Install with `npm install`, run with `npm start`, test with `npm test`.
+
+This version is bounded, so no wrapping around for things like gliders :()
--- server.js
+++ server.js
@@ -15,37 +15,53 @@ app.use(logger())
 app.use(cors())
 
 app.use(function * () {
-  const m = parseInt(this.query.M)
-  const n = parseInt(this.query.N)
+  // parse query
+  const m = parseInt(this.query.M, 10)
+  const n = parseInt(this.query.N, 10)
+  let liveCells = this.query.liveCells
+
+  // calculate simulation bounds
+  const xs = liveCells.map(coord => parseInt(coord[0], 10))
+  const ys = liveCells.map(coord => parseInt(coord[1], 10))
+  const minX = Math.max(0, Math.min(...xs) - 1)
+  const maxX = Math.min(n, Math.max(...xs) + 1)
+  const minY = Math.max(0, Math.min(...ys) - 1)
+  const maxY = Math.min(m, Math.max(...ys) + 1)
+
+  // recreate minimal local copy
   const cells = []
-  for(let y=0; y<m; y++){
-    cells.push(new Array(n).fill(0))
+
+  for (let y = minY; y <= maxY; y++) {
+    cells.push(new Array(maxX - minX + 1).fill(0))
   }
 
-  let liveCells = this.query.liveCells
   for (let [x, y] of liveCells) {
-    cells[parseInt(x)][parseInt(y)] = 1
+    cells[parseInt(y, 10) - minY][parseInt(x, 10) - minX] = 1
   }
+  console.log(cells)
 
   const neighbors = function (x, y) {
     let sum = 0
-    sum += (cells[x-1] && cells[x-1][y-1] ? 1 : 0)
-    sum += (cells[x-1] && cells[x-1][y] || 0)
-    sum += (cells[x-1] && cells[x-1][y+1] ? 1 : 0)
-    sum += (cells[x][y-1] || 0)
-    sum += (cells[x][y+1] || 0)
-    sum += (cells[x+1] && cells[x+1][y-1] ? 1 : 0)
-    sum += (cells[x+1] && cells[x+1][y] ? 1 : 0)
-    sum += (cells[x+1] && cells[x+1][y+1] ? 1 : 0)
+    sum += (cells[y - 1] || [])[x - 1] || 0
+    sum += (cells[y - 1] || [])[x] || 0
+    sum += (cells[y - 1] || [])[x + 1] || 0
+
+    sum += (cells[y] || [])[x - 1] || 0
+    sum += (cells[y] || [])[x + 1] || 0
+
+    sum += (cells[y + 1] || [])[x - 1] || 0
+    sum += (cells[y + 1] || [])[x] || 0
+    sum += (cells[y + 1] || [])[x + 1] || 0
     return sum
   }
 
+  // we only need to simulate the bounds of all the live cells +- 1
   liveCells = []
-  for (let x = 0; x < m; x++) {
-    for (let y = 0; y < n; y++) {
+  for (let y = 0; y < cells.length; y++) {
+    for (let x = 0; x < cells[0].length; x++) {
       const friends = neighbors(x, y)
-      if (friends === 3 || (friends === 2 && cells[x][y])) {
-        liveCells.push([x, y])
+      if (friends === 3 || (friends === 2 && cells[y][x])) {
+        liveCells.push([x + minX, y + minY])
       }
     }
   }
